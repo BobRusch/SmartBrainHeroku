624f8dc124355c42e72f2c3976a73ebd5ae153f8
Bob Rusch <rvrusch@gmail.com>
2021-11-14 11:13:51 -0500

    api=>web

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker login --username $HEROKU_UUSR --password $HEROKU_UPSWD registry.heroku.com
-docker push registry.heroku.com/smb-back/api
-heroku container:release api --app smb-back
+docker push registry.heroku.com/smb-back/web
+heroku container:release web --app smb-back
 docker push registry.heroku.com/sbclient/web
 heroku container:release web --app sbclient
