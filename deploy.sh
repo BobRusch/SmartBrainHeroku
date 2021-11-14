f5aa215527100372a37888fcf677953cd20ba1b7
Bob Rusch <rvrusch@gmail.com>
2021-11-14 08:57:43 -0500

    error in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker login --username $HEROKU_UUSR --password $HEROKU_UPSWD registry.heroku.com
 docker push registry.heroku.com/smb-back/api
 heroku container:release api --app smb-back
-docker push registry.heroku.com/smb-front/web
-heroku container:release web --app smb-front
+docker push registry.heroku.com/sbclient/web
+heroku container:release web --app sbclient
