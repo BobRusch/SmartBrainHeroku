a5f07f3c9f63f165b72617a4dc757ddf717996d0
Bob Rusch <rvrusch@gmail.com>
2021-11-14 11:25:46 -0500

    rm'd login in deploy,  logged in by travis prior

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-docker login --username $HEROKU_UUSR --password $HEROKU_UPSWD registry.heroku.com
 docker push registry.heroku.com/smb-back/web
 heroku container:release web --app smb-back
 docker push registry.heroku.com/sbclient/web
