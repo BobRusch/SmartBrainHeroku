e8df496e8b9e632bd76751d5f2e5ce28a9497e12
Bob Rusch <rvrusch@gmail.com>
2021-11-14 08:36:13 -0500

    smb-front renamed sbclient

--- deploy_api.sh
+++ deploy_api.sh
@@ -1,3 +1,3 @@
 docker login --username $HEROKU_UUSR --password $HEROKU_UPSWD registry.heroku.com
-docker push registry.heroku.com/smb-back/api
-heroku container:release api --app smb-back
+docker push registry.heroku.com/sbclient/api
+heroku container:release api --app sbclient
