3e2f4b797a10cf705e8364854d5eadb35c68ed6b
Bob Rusch <rvrusch@gmail.com>
2021-11-20 16:57:54 -0500

    upd auth

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+echo "$HEROKU_AUTH" | docker login -u "_" --password-stdin registry.heroku.com
 docker push registry.heroku.com/smb-back/web
 heroku container:release web --app $HEROKU_BACK
 docker push registry.heroku.com/sbclient/web
